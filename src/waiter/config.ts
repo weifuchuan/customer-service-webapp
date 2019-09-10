cf1679221c7853561349b1bb29a2dfbbd7863910
fuchuan <fuchuan.wei@hotmail.com>
2019-09-10 17:42:58 +0800

    去掉云服务器信息

--- src/waiter/config.ts
+++ src/waiter/config.ts
@@ -3,5 +3,5 @@ import { GlobalConfig } from "./store";
 export const config: GlobalConfig = {
   url: __DEV__
     ? "ws://127.0.0.1:7777?id=1008601"
-    : "ws://123.207.28.107:7777?id=1008601"
+    : "ws://127.0.0.1:7777?id=1008601"
 };
